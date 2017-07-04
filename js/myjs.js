55f1aac850891b2e0ab609a9059dfdc8063cf5ae
Jessie Nerona <jbnerona@gmail.com>
2017-07-04 15:07:29 +0800

    Portfolio 2

--- js/myjs.js
+++ js/myjs.js
@@ -1,56 +1,61 @@
-$(document).ready(function(){
-	$('.arrow').click(function(){
-	var h = 	$('#navbar').css("height");
-	if(h == "80px"){
-			$('.arrow').html('<img src="img/arrow-up.png">	');	
-		$('#navbar').animate({height:"410px"},  { duration: 500, queue: false });
-		$('#navbar ul').css({"display":"block"});
-	}else{
-		$('#navbar').animate({height:"80px"}, 500, function(){
-			$('#navbar ul').css({"display":"none"});
-			$('.arrow').html('<img src="img/arrow-down.png">	');			
-		});
 
-	}
+    $('#about-link').click(function(){
+      $('#about').css({"display":"block"});
+      $('#left').animate({width:"50%"}, 600);
+      $('#right').animate({width:"50%"}, 600);  
+    })
 
-})
-})
+    $('.close').click(function(){
+      
+      $('#left').animate({width:"0%"}, 600);
+      $('#right').animate({width:"0%"}, 600, function(){
 
+        $('#about').css({"display":"none"});
+      }); 
 
+    })
+    // for ruby
 
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+    $('.ruby').click(function(){
+
+      $('#ruby').animate({height:"100vh"}, 300);    
+        $('#ruby').css({"display":"block"});
+    })
+
+    $('.close-black').click(function(){
+
+      $('#ruby').animate({height:"0vh"}, 300, function(){
+        $('#ruby').css({"display":"none"});
+      });   
+        
+    })
+
+    // for php
+    $('.php').click(function(){
+
+      $('#php').animate({height:"100vh"}, 300);   
+        $('#php').css({"display":"block"});
+    })
+
+    $('.close-black').click(function(){
+
+      $('#php').animate({height:"0vh"}, 300, function(){
+        $('#php').css({"display":"none"});
+      });   
+        
+    })
+    
+    // for html
+    $('.html-css').click(function(){
+
+      $('#html').animate({height:"100vh"}, 300);    
+        $('#html').css({"display":"block"});
+    })
+
+    $('.close-black').click(function(){
+
+      $('#html').animate({height:"0vh"}, 300, function(){
+        $('#html').css({"display":"none"});
+      });   
+        
+    })
