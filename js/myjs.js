c849315cbbf1e8a9c5b7199e8c06fd08af7b77d0
Jessie Nerona <jbnerona@gmail.com>
2017-07-05 10:30:17 +0800

    js

--- js/myjs.js
+++ js/myjs.js
@@ -23,7 +23,7 @@
       $('#portfolio').animate({height:"100vh"}, 300);   
           $('#portfolio').css({"display":"block"}); 
             $('body').css({"overflow":"auto"});
-                        // $('#header').css({"display":"none"});
+                        $('#portfolio').css({"top":"0"});
                             $('#about').hide();
                             $('nav').hide();
 
