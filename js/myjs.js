818a90caf8cc04e34ed80b8648b8795783339240
Jessie Nerona <jbnerona@gmail.com>
2017-07-11 20:48:54 +0800

    old site

--- js/myjs.js
+++ js/myjs.js
@@ -1,25 +1,179 @@
-var clicked = false;
-var clicked_id = "";
 
-$('a').click(function(){
-    var id = '#'+this.className;
-    if(clicked ==  false){ clicked = true; clicked_id = id; }
+    $('#about-link').click(function(){
+      $('#about').css({"display":"block"});
+      $('#left').animate({width:"50%"}, 600);
+      $('#right').animate({width:"50%"}, 600);  
+    })
 
-    $(clicked_id).css({ "zIndex":"1000"}).animate({borderBottomWidth:"0px", borderTopWidth:"0px"}).slideUp("slow", function(){
-         clicked_id = id;
-         $(id).slideDown("slow", function(){
-            $(id).css({"borderTop":"0px solid rgba(0, 0, 0, 1)", "borderBottom":"0px solid rgba(0, 0, 0, 1)"}).animate({borderBottomWidth:"105px", borderTopWidth:"15px", zIndex:"31"});
-        });
+    $('.close').click(function(){
+      
+      $('#left').animate({width:"0%"}, 600);
+      $('#right').animate({width:"0%"}, 600, function(){
 
-     });
+        $('#about').css({"display":"none"});
+      }); 
 
-});
+    })
+    // for ruby
+
+    $('.portfolio').click(function(){
 
 
-$('.home').click(function(){
-      $('#about, #tools, #works, #contacts').css({ "zIndex":"1000"}).animate({borderBottomWidth:"0px", borderTopWidth:"0px"}).slideUp();
-      clicked = false;
-});
+           
+      $('#portfolio').animate({height:"100vh"}, 600);   
+          $('#portfolio').css({"display":"block"}); 
+            // $('#portfolio').css({"overflow":"auto"});
+                        // $('#portfolio').css({"top":"0"});
+                            $('#about').hide();
+                            $('nav').hide();
+                            $('#footer').hide()
+
+    })
+
+    $('.close-m').click(function(){
+         // $('#header').css({"display":"block"});
+         //          $('#left').css({"display":"block"});
+               // $('#about').css({"display":"block"});
+                   $('#about').show();
+                     $('nav').show();
+                     $('#footer').show()
+          $('#portfolio').animate({height:"0vh"}, 600, function(){
+        $('#portfolio').css({"display":"none"});
+                     $('.ruby-tab').css({"background-color":"rgba(255,255,255,0)", "color":"#000"});
+      $('.web-tab, .other-tab').css({"background-color":"#000", "color":"#fff"});
+      $("#ruby").show();
+      $('#web, #other').hide(); 
+
+      });   
+       
+
+    })
+
+        $('.close-x').click(function(){
+                         $('nav').show();
+                         $('#footer').show()
+                         $('body').css({"overflow":"hidden"});
+          $('#portfolio').animate({height:"0vh"}, 600, function(){
+        $('#portfolio').css({"display":"none"});
+
+      });   
+        
+    })
+
+    // for skills
+    $('.skills').click(function(){
+
+      $('#skills').animate({height:"100vh"}, 600);   
+        $('#skills').css({"display":"block"});
+        $('#about').hide();
+        $('#footer').hide()
+          $('nav').hide();
+           // $('#header').css({"display":"none"});
+           //                  $('#about').css({"display":"none"});
+    })
+
+    $('.close-m').click(function(){
+            // $('#header').css({"display":"block"});
+            //      $('#about').css({"display":"block"});
+           $('#about').show();
+             $('nav').show();
+             $('#footer').show()
+      $('#skills').animate({height:"0vh"}, 600, function(){
+        $('#skills').css({"display":"none"});
+      });   
+        
+    })
+
+        $('.close-x').click(function(){
+$('#footer').show()
+             $('nav').show();
+      $('#skills').animate({height:"0vh"}, 600, function(){
+        $('#skills').css({"display":"none"});
+      });   
+        
+    })
+    
+    $(document).scroll(function(){
+      $('.scroll-up').css({"display":"block"});
+              $('.scroll-down').css({"display":"block"});     
+      var a = $(document).scrollTop();
+      if (a == "0"){
+             $('.scroll-up').css({"display":"none"});
+             $('.scroll-down').css({"display":"none"});
+      }
+
+      var scroll_down = $(document).height() - $(window).height();
+            if ($(document).scrollTop() == scroll_down){
+                $('.scroll-down').css({"display":"none"});
+      }
+    })
+
+    $('.scroll-up').click(function(){
+      $('body').stop().animate({scrollTop:0}, 500, 'swing', function(){
+        // alert('it worked!');
+      });
+    });
+
+        $('.scroll-down').click(function(){
+      $('body').stop().animate({scrollTop: $(document).height() - $(window).height() }, 500, 'swing', function(){
+        // alert('it worked!');
+      });
+    });
+
+       $('.arrow-down').click(function(){
+      $('html, body').animate({scrollTop: $('#header').height() }, 500);
+    });
+
+    $('#portfolio').scroll(function(){
+      $('.p-scroll-up').css({"display":"block"});
+            $('.p-scroll-down').css({"display":"block"});
+      var a = $('#portfolio').scrollTop();
+      if (a == "0"){
+             $('.p-scroll-up').css({"display":"none"});
+                   $('.p-scroll-down').css({"display":"none"});
+      }
+      var scroll_down = $(document).height() - $(window).height();
+            if ($('#portfolio').scrollTop() == scroll_down){
+                $('.p-scroll-down').css({"display":"none"});
+      }
+
+    })
+
+    $('.p-scroll-up').click(function(){
+      $('#portfolio').stop().animate({scrollTop:0}, 500, 'swing', function(){
+        // alert('it worked!');
+      });
+    });
+
+        $('.p-scroll-down').click(function(){
+      $('#portfolio').stop().animate({scrollTop: $(document).height()}, 500, 'swing', function(){
+        // alert('it worked!');
+      });
+    });
+
+    // ######
+    // tab here
+    // ######
+    $('.web-tab').click(function(){
+      $('.web-tab').css({"background-color":"rgba(255,255,255,0)", "color":"#000"});
+      $('.ruby-tab, .other-tab').css({"background-color":"#000", "color":"#fff"});
+      $("#web").show();
+      $('#ruby, #other').hide();
+    });
+
+    $('.ruby-tab').click(function(){
+      $('.ruby-tab').css({"background-color":"rgba(255,255,255,0)", "color":"#000"});
+      $('.web-tab, .other-tab').css({"background-color":"#000", "color":"#fff"});
+      $("#ruby").show();
+      $('#web, #other').hide();
+    });
+
+    $('.other-tab').click(function(){
+     $('.other-tab').css({"background-color":"rgba(255,255,255,0)", "color":"#000"});
+           $('.ruby-tab, .web-tab').css({"background-color":"#000", "color":"#fff"});
+      $("#other").show();
+      $('#ruby, #web').hide();
+    });
 
 
     // particles
